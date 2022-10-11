a169274c01241b5b21c3aa70370787a74d86cfd8
OJII3 <kawaisounayuriko@gmail.com>
2022-10-11 12:54:23 +0900

    Change js to ts

--- line_api.js
+++ line_api.js
@@ -1,51 +1,45 @@
 "use strict";
-
-const https = require("https");
-module.exports = new lineApi();
-
-
-class lineApi {
-  
-  // property
-  messages = [];
-  
-  // constructor
-  constructor() {}
-  
-  // readonly fields
-  #accessToken = process.env.LINE_ACCESS_TOKEN;
-  #userId = process.env.LINE_USER_ID;
-  #pushUrl = "https://api.line.me/v2/bot/message/push";
-  
-  // meethods
-  addMessage(message) {
-    const messageSizeLimit = 5; // LINE API limit
-    if(this.messages.length < messageSizeLimit) {
-      this.messages.push(message);
-    } else {
-      console.error("Too many messages");
+var https = require("https");
+var line_api = /** @class */ (function () {
+    // constructor
+    function line_api() {
+        // property
+        this.messages = [];
     }
-  }
-  
-  sendPushMessage() {
-    const options = {
-      hostname: "https://api.line.me",
-      path: "v2/bot/message/push",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${this.#accessToken}`
-      }
+    // readonly fields
+    // meethods
+    line_api.prototype.addMessage = function (message) {
+        var messageSizeLimit = 5; // LINE API limit
+        if (this.messages.length < messageSizeLimit) {
+            this.messages.push(message);
+        }
+        else {
+            console.error("Too many messages");
+        }
     };
-    const data = {
-      to: this.#userId,
-      messages: this.messages
+    line_api.prototype.sendPushMessage = function () {
+        var accessToken = process.env.LINE_ACCESS_TOKEN;
+        var userId = process.env.LINE_USER_ID;
+        var options = {
+            hostname: "https://api.line.me",
+            path: "v2/bot/message/push",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer ".concat(accessToken)
+            }
+        };
+        var data = {
+            to: userId,
+            messages: this.messages
+        };
+        var req = https.request(options, function (res) { });
+        req.write(JSON.stringify(data));
+        req.on("error", function (err) { return console.log(err); });
+        req.end();
     };
-    
-    const req = https.request(options, (res) => {
-    });
-    req.write(JSON.stringify(data));
-    req.on("error", (err) => console.log(err));
-    req.end();
-  }
-}
+    return line_api;
+}());
+// export an instance as a module
+// I'm not sure what object oriented js should be like
+module.exports = new line_api();
